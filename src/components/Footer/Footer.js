5d5d5c8e102b393561b37d1d9f0228735edacb8a
Marco Ye <marco.ye99@gmail.com>
2022-08-01 19:26:23 -0400

    finished general layout; additional styling required

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,18 @@
+import './Footer.scss'
+
 function Footer() {
     return (
-        <>
-        </>
+        <footer className="footer">
+            <ul className="footer__list">
+                <li className="footer__item">
+                    About
+                </li>
+                <li className="footer__item">
+                    Contact
+                </li>
+
+            </ul>
+        </footer>
     );
 }
 
