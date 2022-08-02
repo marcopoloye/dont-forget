b4147dd5e3d6cf7feb2a262ba6c27775e1042c0c
Marco Ye <marco.ye99@gmail.com>
2022-08-01 21:30:40 -0400

    added searchbar

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import SearchBar from '../components/SearchBar/SearchBar';
 
 class HomePage extends Component {
     state = {  } 
     render() { 
         return (
             <>
+                <SearchBar />
             </>
         );
     }
