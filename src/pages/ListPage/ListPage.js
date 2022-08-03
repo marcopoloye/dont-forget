08d229b4a66f12ce7dfb31dd4b5fb422c90831c0
Marco Ye <104222379+marcopoloye@users.noreply.github.com>
2022-08-03 15:45:35 -0400

    Create ListPage.js

---
+++ src/pages/ListPage/ListPage.js
@@ -0,0 +1,18 @@
+import './ListPage.scss';
+import SuggestForm from '../../components/SuggestForm/SuggestForm';
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
+
+function ListPage() {
+    useEffect(() => {
+
+    })
+    
+    return (
+        <>
+            <SuggestForm />
+        </>
+    );
+}
+
+export default ListPage;
