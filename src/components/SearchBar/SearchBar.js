b3949d2a24ee46d17413dbc7666e8921743c912b
Marco Ye <marco.ye99@gmail.com>
2022-08-03 19:06:41 -0400

    new api key

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -3,20 +3,32 @@ import axios from 'axios';
 
 function SearchBar() {
 
+    const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        const searchValue = e.target[0].value
+        const searchValue = e.target[0].value;
 
         if (!searchValue) {
             console.log('empty');
         } else {
             axios
-            .get(`http://api.openweathermap.org/geo/1.0/direct?q=${e.target[0].value}&appid=94659958f8437b131fd51734fb5ebf76`)
+            .get(`http://api.openweathermap.org/geo/1.0/direct?q=${e.target[0].value}&appid=${API_KEY}`)
             .then (result => {
+                console.log(result)
                 const latitude = result.data[0].lat;
                 const longitude = result.data[0].lon;
-                console.log(searchValue, latitude, longitude)
+                axios
+                    .get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
+                    .then (result => {
+                        console.log('id', result.data.id)
+                        const temperature = Math.round((result.data.main.temp) - 273.15)
+                        console.log(temperature)
+                    })
+                    .catch(error => {
+                        console.log('Error')
+                    })
             })
             .catch(error => {
                 console.log('Error getting destination')
