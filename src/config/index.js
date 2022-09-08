87ac32b528a57eb2c8b54e0a9949c9478f157a8e
Marco Ye <marco.ye99@gmail.com>
2022-09-07 20:08:45 -0400

    modified heroku url

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
 export const API_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://dont-forget-2022.herokuapp.com'
+    ? 'https://dont-forget-2022-backend.herokuapp.com'
     : 'http://localhost:3000';
