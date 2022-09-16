08f1a6f5848d74f6005de4254a3e6ecfa0a936c7
Marco Ye <marco.ye99@gmail.com>
2022-09-16 19:47:44 -0400

    fixed path

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import React, { useState} from 'react';
 import ChecklistForm from '../ChecklistForm/ChecklistForm';
 import ChecklistList from '../ChecklistList/ChecklistList';
-import { API_URL } from '../../config';'../../config/index';
+import { API_URL } from '../../config/index';
 
 function SearchBar({locationInput, setLocationInput}) {
     const [inputText, setInputText] = useState('');
